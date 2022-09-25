04ccc1aaf1ce108a85ce037b510e3132d1d9aa75
minidam <minidamjp@ikedam.jp>
2022-09-25 18:07:19 +0900

    npm run ng -- generate component WeightGraph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopPageComponent } from './top-page/top-page.component';
+import { WeightGraphComponent } from './weight-graph/weight-graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopPageComponent
+    TopPageComponent,
+    WeightGraphComponent
   ],
   imports: [
     BrowserModule,
